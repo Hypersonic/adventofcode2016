09b397315bd1a20e674ec129293429cf4f4f9ba9
Josh Hofing <joshhofing@gmail.com>
2016-12-14 01:49:43 -0500

    Generate to 20k ahead-of-time for godspeed

--- day14/solve.py
+++ day14/solve.py
@@ -11,22 +11,28 @@ def memoize(f):
             return ret
     return memodict(f)
 
-@memoize
 def md5(x):
     return hashlib.md5(x).hexdigest().lower()
 
 #part 1
 """
-def hashing(x):
+def hashing_nomemo(x):
     return md5(x)
+
+@memoize
+def hashing(x):
+    return hashing_nomemo(x)
 """
 
 # part 2
-@memoize
-def hashing(x):
+def hashing_nomemo(x):
     for i in range(2017):
         x = md5(x)
     return x
+
+@memoize
+def hashing(x):
+    return hashing_nomemo(x)
 
 salt = 'yjdafjpo'
 #salt = 'abc'
@@ -43,9 +49,19 @@ def is_valid(n):
     i = min(triplets, key=lambda i: hashing(salt+str(n)).index(str(i)*3))
     for j in range(1, 1001):
         if str(i)*5 in hashing(salt+str(n+j)):
-            print hashing(salt+str(n+j)), hashing(salt+str(n)), i
+            #print hashing(salt+str(n+j)), hashing(salt+str(n)), i
             return True
     return False
+
+print 'Pregenerating 20000 hashes...'
+import multiprocessing, time
+start = time.time()
+to_hash = [salt + str(i) for i in range(20000)]
+for i,hsh in enumerate(multiprocessing.Pool().map(hashing_nomemo, to_hash)):
+    hashing[salt+str(i)] = hsh
+end = time.time()
+print 'took', end-start
+print 'ok... now to the real work!!!'
 
 from itertools import count
 n = 0
